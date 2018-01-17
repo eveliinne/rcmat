f73ca5fb6591a994a0dc0367ca776b10ba6a0210
eveliinne <34340040+eveliinne@users.noreply.github.com>
2018-01-17 20:15:29 +0100

    Add files via upload

---
+++ stack.h
@@ -0,0 +1,19 @@
+#pragma once
+
+class stack
+{
+public:
+	void push(int a);
+	int pop();
+	void clear();
+	void print();
+	int isempty();
+	stack();
+	~stack();
+private:
+	int top;
+	int *stos;
+};
+
+
+
